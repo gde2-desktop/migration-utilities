3cfcb69ca2c8b26ccfdc39ae4fc28235fc459d57
Михалыч <gtk3@inbox.ru>
2020-12-01 17:42:39 +0500

    update libmatemixer script

--- replace-libmatemixer.sh
+++ replace-libmatemixer.sh
@@ -3,9 +3,13 @@
 
 srcdir="$1"
 find "$srcdir" -type f -exec \
-sed -i 's/GDE2MIXER/MATEMIXER/g' {} + -print
+sed -i 's/GDE2_MIXER/MATE_MIXER/g' {} + -print
 
 find "$srcdir" -type f -exec \
-sed -i 's/gde2mixer/matemixer/g' {} + -print
+sed -i 's/gde2_mixer/mate_mixer/g' {} + -print
+
+find "$srcdir" -type f -exec \
+sed -i 's/Gde2Mixer/MateMixer/g' {} + -print
+
 
 
